1a6d767e3ddece0b073c4dd4b5f6e122f5a27cbb
3740 <xianzhi.chen@hand-china.com>
2017-08-23 18:36:10 +0800

    新增管理公司功能

---
+++ src/main/java/com/ey/service/system/mgrcompany/impl/MgrcompanyService.java
@@ -0,0 +1,82 @@
+package com.ey.service.system.mgrcompany.impl;
+
+import java.util.List;
+import javax.annotation.Resource;
+import org.springframework.stereotype.Service;
+import com.ey.dao.DaoSupport;
+import com.ey.entity.Page;
+import com.ey.util.PageData;
+import com.ey.service.system.mgrcompany.MgrcompanyManager;
+
+/** 
+ * 说明： 管理公司信息
+ * 创建人：andychen
+ * 创建时间：2017-08-23
+ * @version
+ */
+@Service("mgrcompanyService")
+public class MgrcompanyService implements MgrcompanyManager{
+
+	@Resource(name = "daoSupport")
+	private DaoSupport dao;
+	
+	/**新增
+	 * @param pd
+	 * @throws Exception
+	 */
+	public void save(PageData pd)throws Exception{
+		dao.save("MgrcompanyMapper.save", pd);
+	}
+	
+	/**删除
+	 * @param pd
+	 * @throws Exception
+	 */
+	public void delete(PageData pd)throws Exception{
+		dao.delete("MgrcompanyMapper.delete", pd);
+	}
+	
+	/**修改
+	 * @param pd
+	 * @throws Exception
+	 */
+	public void edit(PageData pd)throws Exception{
+		dao.update("MgrcompanyMapper.edit", pd);
+	}
+	
+	/**列表
+	 * @param page
+	 * @throws Exception
+	 */
+	@SuppressWarnings("unchecked")
+	public List<PageData> list(Page page)throws Exception{
+		return (List<PageData>)dao.findForList("MgrcompanyMapper.datalistPage", page);
+	}
+	
+	/**列表(全部)
+	 * @param pd
+	 * @throws Exception
+	 */
+	@SuppressWarnings("unchecked")
+	public List<PageData> listAll(PageData pd)throws Exception{
+		return (List<PageData>)dao.findForList("MgrcompanyMapper.listAll", pd);
+	}
+	
+	/**通过id获取数据
+	 * @param pd
+	 * @throws Exception
+	 */
+	public PageData findById(PageData pd)throws Exception{
+		return (PageData)dao.findForObject("MgrcompanyMapper.findById", pd);
+	}
+	
+	/**批量删除
+	 * @param ArrayDATA_IDS
+	 * @throws Exception
+	 */
+	public void deleteAll(String[] ArrayDATA_IDS)throws Exception{
+		dao.delete("MgrcompanyMapper.deleteAll", ArrayDATA_IDS);
+	}
+	
+}
+
