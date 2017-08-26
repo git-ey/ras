2f0fe8e262220d363fcfce3dea126246e1999cc0
3740 <xianzhi.chen@hand-china.com>
2017-08-26 22:38:51 +0800

    简化导出C的控制器

--- src/main/java/com/ey/controller/pbc/output/CExportController.java
+++ src/main/java/com/ey/controller/pbc/output/CExportController.java
@@ -13,42 +13,36 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.ey.controller.base.BaseController;
 import com.ey.service.pbc.output.CExportManager;
+import com.ey.util.PageData;
 
-/** 
- * 说明： 底稿输出测试Controller
- * 创建人：Dai Zong
- * 创建时间：2017-08-25
+/**
+ * 说明： 底稿输出测试Controller 创建人：Dai Zong 创建时间：2017-08-25
  */
 @Controller
-@RequestMapping(value="/export")
+@RequestMapping(value = "/export")
 public class CExportController extends BaseController {
-	@Resource(name="cExportService")
-    private CExportManager cExportService;
-	
-    	@InitBinder
-	public void initBinder(WebDataBinder binder){
+	@Resource(name = "cExportService")
+	private CExportManager cExportService;
+
+	@InitBinder
+	public void initBinder(WebDataBinder binder) {
 		DateFormat format = new SimpleDateFormat("yyyy-MM-dd");
-		binder.registerCustomEditor(Date.class, new CustomDateEditor(format,true));
+		binder.registerCustomEditor(Date.class, new CustomDateEditor(format, true));
 	}
-	
-	 /**导出到excel
-     * @param
-     * @throws Exception
-     */
-    @RequestMapping(value="/c",method=RequestMethod.GET, produces="application/x-www-form-urlencoded;charset=UTF-8")
-    public void output(HttpServletRequest request, HttpServletResponse response){
-        try {
-        	// 基金ID
-        	String fundId =  request.getParameter("FUND_ID");
-        	// 期间
-        	String peroid =  request.getParameter("PEROID");
-            this.cExportService.doExport(request, response, fundId, Long.parseLong(peroid));
-        } catch (Exception e) {
-            this.logger.error("", e);
-        }
-    }
+
+	/**
+	 * 导出到excel
+	 * 
+	 * @param
+	 * @throws Exception
+	 */
+	@RequestMapping(value = "/c")
+	public void output(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		PageData pd = this.getPageData();
+		this.cExportService.doExport(request, response, pd.getString("FUND_ID"),
+				Long.parseLong(pd.getString("PEROID")));
+	}
 }
