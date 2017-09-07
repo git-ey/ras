65bfcf1dbec448ba42976e6dee8059a034ea335d
10902 <gaokuo.dai@hand-china.com>
2017-09-07 23:58:57 +0800

    [add]开发G300导出页

--- src/main/java/com/ey/service/wp/output/impl/GExportService.java
+++ src/main/java/com/ey/service/wp/output/impl/GExportService.java
@@ -1,5 +1,6 @@
 package com.ey.service.wp.output.impl;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -36,6 +37,7 @@ public class GExportService implements GExportManager{
         dataMap.put("fundId", fundId);
         
         dataMap.put("G", this.getGData(fundId, period));
+        dataMap.put("G300", this.getG300Data(fundId, period));
         
         String xmlStr = FreeMarkerUtils.processTemplateToString(dataMap, Constants.EXPORT_TEMPLATE_FOLDER_PATH, Constants.EXPORT_TEMPLATE_FILE_NAME_G);
         FileExportUtils.writeFileToHttpResponse(request, response, "G.xls", xmlStr);
@@ -66,6 +68,36 @@ public class GExportService implements GExportManager{
         
         result.put("KM1221", KM1221);
         result.put("KM1501", KM1501);
+        
+        return result;
+    }
+    
+    private Map<String,Object> getG300Data(String fundId, Long period) throws Exception{
+        Map<String, Object> queryMap = new HashMap<String,Object>();
+        Map<String, Object> result = new HashMap<String,Object>();
+        
+        List<Map<String, Object>> KM1221 = new ArrayList<>();
+        List<Map<String, Object>> KM1501 = new ArrayList<>(); 
+        
+        queryMap.put("fundId", fundId);
+        queryMap.put("period", period);
+        @SuppressWarnings("unchecked")
+        List<Map<String,Object>> metaDataList = (List<Map<String,Object>>)dao.findForList("GExportMapper.selectG300Data", queryMap);
+        if(CollectionUtils.isEmpty(metaDataList)) {return result;}
+        
+        for(Map<String,Object> map : metaDataList) {
+            if("1221".equals(map.get("accountNum"))) {
+                KM1221.add(map);
+            }
+            if("1501".equals(map.get("accountNum"))) {
+                KM1501.add(map);
+            }
+        }
+        
+        result.put("KM1221", KM1221);
+        result.put("KM1221Count", KM1221.size());
+        result.put("KM1501", KM1501);
+        result.put("KM1501Count", KM1501.size());
         
         return result;
     }
